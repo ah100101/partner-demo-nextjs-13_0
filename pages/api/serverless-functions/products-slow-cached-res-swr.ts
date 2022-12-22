5034743effb05a25bd58d807479dade6ab047502
Alex <alex.hawley@vercel.com>
2022-12-22 17:13:33 -0600

    Added swr example of serverless function

---
+++ pages/api/serverless-functions/products-slow-cached-res-swr.ts
@@ -0,0 +1,14 @@
+import { getProducts, Product } from "lib/mocks/products";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Product[]>
+) {
+  const products = await getProducts();
+  res.setHeader(
+    "Cache-Control",
+    "max-age=0, s-maxage=10 stale-while-revalidate"
+  );
+  res.status(200).json(products);
+}
