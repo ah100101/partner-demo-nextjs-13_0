5e588d113849343373559ccf5348722908afc2f7
Alex <alex.hawley@vercel.com>
2022-12-22 17:12:18 -0600

    Removed random synchronous timing

--- lib/mocks/simulate-synchronous-work.ts
+++ lib/mocks/simulate-synchronous-work.ts
@@ -1,8 +1,5 @@
 const simulateSynchronousWork = () => {
-  return new Promise((resolve) =>
-    // wait between 1 - 3 seconds
-    setTimeout(resolve, (1 + Math.random() * 2) * 1000)
-  );
+  return new Promise((resolve) => setTimeout(resolve, 2000));
 };
 
 export default simulateSynchronousWork;
