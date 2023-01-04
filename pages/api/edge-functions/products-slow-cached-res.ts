f091b24f001b22d5707ab0424d6f06a3d59bfd9f
Alex <alex.hawley@vercel.com>
2023-01-03 18:08:41 -0600

    removed swr

--- pages/api/edge-functions/products-slow-cached-res.ts
+++ pages/api/edge-functions/products-slow-cached-res.ts
@@ -7,11 +7,11 @@ export const config = {
 
 const products = async (req: NextRequest) => {
   const products = await getProducts();
-  // cache-control > browser should not cache, edge cache for 5 seconds
+  // cache-control > browser should not cache, edge cache for 10 seconds
   return NextResponse.json(products, {
     status: 200,
     headers: {
-      "Cache-Control": "max-age=0 s-maxage=5",
+      "Cache-Control": "max-age=0 s-maxage=10",
     },
   });
 };
