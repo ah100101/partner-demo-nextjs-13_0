331fc7d7a3212a8947e7b577688a49b94a841e38
Alex <alex.hawley@vercel.com>
2023-01-06 13:42:43 -0600

    added logic for reading body from request in middleware

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// config with custom matcher
+export const config = {
+  matcher: "/:path*",
+};
+
+export default async function middleware(
+  request: NextRequest,
+  res: NextResponse
+) {
+  const reader = request?.body?.getReader();
+  if (!reader) return;
+  const decoder = new TextDecoder("utf-8");
+  let body = "";
+  let { done, value } = await reader.read();
+  while (!done) {
+    body += decoder.decode(value);
+    ({ done, value } = await reader.read());
+  }
+  return NextResponse.json(body);
+}
