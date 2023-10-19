30ecd112c5225bb1cc714dbf74c838a387eba2a7
Alex <alex.hawley@vercel.com>
2023-10-19 11:22:41 -0500

    fixed response return

--- app/api/webhook/deployment-succeeded/route.ts
+++ app/api/webhook/deployment-succeeded/route.ts
@@ -1,5 +1,7 @@
 export async function POST(request: Request) {
   const res = await request.json();
   console.log({ res });
-  return Response.json({ res });
+  return new Response(JSON.stringify({ res }), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
