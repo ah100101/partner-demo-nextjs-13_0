e25b28ed5c6501f0c55d8a66def1f56d231e7caa
Alex <alex.hawley@vercel.com>
2023-10-19 11:49:10 -0500

    added headers to res

--- app/api/webhook/deployment-succeeded/route.ts
+++ app/api/webhook/deployment-succeeded/route.ts
@@ -1,7 +1,22 @@
+const crypto = require("crypto");
+
+async function verifySignature(request: Request) {
+  const payload = await request.text();
+  const signature = crypto
+    .createHmac("sha1", process.env.WEBHOOK_SECRET)
+    .update(payload)
+    .digest("hex");
+  return signature === request.headers.get("x-vercel-signature");
+}
+
 export async function POST(request: Request) {
+  const headers = Array.from(request.headers.entries());
+
   const res = await request.json();
-  console.log({ res });
-  return new Response(JSON.stringify({ res }), {
+
+  console.log({ res, headers });
+
+  return new Response(JSON.stringify({ res, headers }), {
     headers: { "Content-Type": "application/json" },
   });
 }
